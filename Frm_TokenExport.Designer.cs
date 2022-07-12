f5984a090835512eff2f3687c48cc6350c4f172e
shan333chao@qq.com <shan333chao@qq.com>
2022-07-12 23:07:20 +0800

    添加异步导入导出 导入时会 结束adb进程

--- Frm_TokenExport.Designer.cs
+++ Frm_TokenExport.Designer.cs
@@ -41,11 +41,13 @@
             this.txt_vms_folder = new System.Windows.Forms.TextBox();
             this.txt_export_path = new System.Windows.Forms.TextBox();
             this.txt_log = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btn_box_folder
             // 
-            this.btn_box_folder.Location = new System.Drawing.Point(369, 59);
+            this.btn_box_folder.Location = new System.Drawing.Point(328, 12);
             this.btn_box_folder.Name = "btn_box_folder";
             this.btn_box_folder.Size = new System.Drawing.Size(123, 23);
             this.btn_box_folder.TabIndex = 0;
@@ -55,7 +57,7 @@
             // 
             // btn_export_token
             // 
-            this.btn_export_token.Location = new System.Drawing.Point(129, 237);
+            this.btn_export_token.Location = new System.Drawing.Point(88, 158);
             this.btn_export_token.Name = "btn_export_token";
             this.btn_export_token.Size = new System.Drawing.Size(75, 23);
             this.btn_export_token.TabIndex = 1;
@@ -65,7 +67,7 @@
             // 
             // btn_import_token
             // 
-            this.btn_import_token.Location = new System.Drawing.Point(210, 237);
+            this.btn_import_token.Location = new System.Drawing.Point(169, 158);
             this.btn_import_token.Name = "btn_import_token";
             this.btn_import_token.Size = new System.Drawing.Size(75, 23);
             this.btn_import_token.TabIndex = 2;
@@ -75,7 +77,7 @@
             // 
             // btn_select_export
             // 
-            this.btn_select_export.Location = new System.Drawing.Point(369, 157);
+            this.btn_select_export.Location = new System.Drawing.Point(328, 110);
             this.btn_select_export.Name = "btn_select_export";
             this.btn_select_export.Size = new System.Drawing.Size(123, 23);
             this.btn_select_export.TabIndex = 3;
@@ -86,7 +88,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(55, 59);
+            this.label1.Location = new System.Drawing.Point(14, 12);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(68, 17);
             this.label1.TabIndex = 4;
@@ -95,7 +97,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(55, 157);
+            this.label3.Location = new System.Drawing.Point(14, 110);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(56, 17);
             this.label3.TabIndex = 6;
@@ -104,7 +106,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(55, 121);
+            this.label4.Location = new System.Drawing.Point(14, 74);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(35, 17);
             this.label4.TabIndex = 7;
@@ -113,7 +115,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(55, 91);
+            this.label5.Location = new System.Drawing.Point(14, 44);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(35, 17);
             this.label5.TabIndex = 8;
@@ -121,23 +123,25 @@
             // 
             // cb_start
             // 
+            this.cb_start.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cb_start.FormattingEnabled = true;
-            this.cb_start.Location = new System.Drawing.Point(129, 88);
+            this.cb_start.Location = new System.Drawing.Point(88, 41);
             this.cb_start.Name = "cb_start";
             this.cb_start.Size = new System.Drawing.Size(97, 25);
             this.cb_start.TabIndex = 9;
             // 
             // cb_end
             // 
+            this.cb_end.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cb_end.FormattingEnabled = true;
-            this.cb_end.Location = new System.Drawing.Point(129, 121);
+            this.cb_end.Location = new System.Drawing.Point(88, 74);
             this.cb_end.Name = "cb_end";
             this.cb_end.Size = new System.Drawing.Size(97, 25);
             this.cb_end.TabIndex = 10;
             // 
             // txt_vms_folder
             // 
-            this.txt_vms_folder.Location = new System.Drawing.Point(129, 59);
+            this.txt_vms_folder.Location = new System.Drawing.Point(88, 12);
             this.txt_vms_folder.Name = "txt_vms_folder";
             this.txt_vms_folder.Size = new System.Drawing.Size(234, 23);
             this.txt_vms_folder.TabIndex = 11;
@@ -145,7 +149,7 @@
             // 
             // txt_export_path
             // 
-            this.txt_export_path.Location = new System.Drawing.Point(129, 157);
+            this.txt_export_path.Location = new System.Drawing.Point(88, 110);
             this.txt_export_path.Name = "txt_export_path";
             this.txt_export_path.Size = new System.Drawing.Size(234, 23);
             this.txt_export_path.TabIndex = 12;
@@ -155,18 +159,39 @@
             // 
             this.txt_log.AcceptsReturn = true;
             this.txt_log.AcceptsTab = true;
-            this.txt_log.Location = new System.Drawing.Point(12, 300);
+            this.txt_log.Location = new System.Drawing.Point(14, 211);
             this.txt_log.Multiline = true;
             this.txt_log.Name = "txt_log";
             this.txt_log.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txt_log.Size = new System.Drawing.Size(767, 138);
+            this.txt_log.Size = new System.Drawing.Size(774, 394);
             this.txt_log.TabIndex = 13;
             // 
-            // Form1
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 17);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "日志：";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.IndianRed;
+            this.label6.Location = new System.Drawing.Point(463, 31);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(325, 68);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "说明\r\n1、开始/结束 为模拟器id\r\n2、导出时不需要开启模拟器\r\n3、导入时需要先开启模拟器，并等待进入android系统桌面";
+            // 
+            // Frm_TokenExport
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(798, 611);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.txt_log);
             this.Controls.Add(this.txt_export_path);
             this.Controls.Add(this.txt_vms_folder);
@@ -180,8 +205,8 @@
             this.Controls.Add(this.btn_import_token);
             this.Controls.Add(this.btn_export_token);
             this.Controls.Add(this.btn_box_folder);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "Frm_TokenExport";
+            this.Text = "Ro 模拟器账号导出";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -202,5 +227,7 @@
         private System.Windows.Forms.TextBox txt_vms_folder;
         private System.Windows.Forms.TextBox txt_export_path;
         private System.Windows.Forms.TextBox txt_log;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
     }
 }
--- Frm_TokenExport.cs
+++ Frm_TokenExport.cs
@@ -20,6 +20,7 @@ namespace RO_TOKEN
     public partial class Frm_TokenExport : Form
     {
         private String LDCONSOLE = String.Empty;
+        private String ADBAPP = String.Empty;
         public Frm_TokenExport()
         {
             InitializeComponent();
@@ -29,8 +30,10 @@ namespace RO_TOKEN
 
         public void init()
         {
+            TextBox.CheckForIllegalCrossThreadCalls = false;
+
             SetupHelper.RegisterAssembly(typeof(ExtFileSystem).Assembly);
-            SetupHelper.RegisterAssembly(typeof(DiscUtils.Vmdk.Disk).Assembly);
+            SetupHelper.RegisterAssembly(typeof(Disk).Assembly);
             SetupHelper.RegisterAssembly(typeof(VirtualDiskManager).Assembly);
             SetupHelper.RegisterAssembly(typeof(VirtualDisk).Assembly);
 
@@ -50,16 +53,18 @@ namespace RO_TOKEN
             if (File.Exists(Path.Join(txt_vms_folder.Text, "ls2console.exe")))
             {
                 LDCONSOLE = Path.Join(txt_vms_folder.Text, "ls2console.exe");
+                ADBAPP = Path.Join(txt_vms_folder.Text, "adb.exe");
 
             }
             else if (File.Exists(Path.Join(txt_vms_folder.Text, "ldconsole.exe")))
             {
 
                 LDCONSOLE = Path.Join(txt_vms_folder.Text, "ldconsole.exe");
+                ADBAPP = Path.Join(txt_vms_folder.Text, "adb.exe");
             }
             else
             {
-                MessageBox.Show("模拟器路径选择错误");
+                txt_log.AppendText("\r\n[模拟器目录]选择不正确");
                 return false;
 
             }
@@ -82,12 +87,12 @@ namespace RO_TOKEN
 
         private void btn_select_export_Click(object sender, EventArgs e)
         {
-            FolderBrowserDialog dilog = new FolderBrowserDialog(); 
+            FolderBrowserDialog dilog = new FolderBrowserDialog();
             dilog.Description = "选择导出目录";
             var result = dilog.ShowDialog();
-            if (result == DialogResult.OK || result == DialogResult.Yes) 
-            { 
-                txt_export_path.Text = dilog.SelectedPath; 
+            if (result == DialogResult.OK || result == DialogResult.Yes)
+            {
+                txt_export_path.Text = dilog.SelectedPath;
             }
         }
 
@@ -114,9 +119,19 @@ namespace RO_TOKEN
             return filenames;
         }
 
-        private void btn_export_token_Click(object sender, EventArgs e)
+        private async void btn_export_token_Click(object sender, EventArgs e)
         {
+            if (!Directory.Exists(txt_export_path.Text))
+            {
+                txt_log.AppendText("\r\n [导出目录]不存在");
+                return;
+            }
+            bool init_ld = Init_Ldconsole();
 
+            if (!init_ld)
+            {
+                return;
+            }
             HashSet<String> allVmdkFiles = getVmdkFiles();
             for (int i = cb_start.SelectedIndex; i <= cb_end.SelectedIndex; i++)
             {
@@ -136,7 +151,7 @@ namespace RO_TOKEN
                 txt_log.AppendText("\r\n" + "导出：" + exportFileName);
                 try
                 {
-                    ExportToken(vmdk, exportFileName);
+                    await ExportToken(vmdk, exportFileName);
                 }
                 catch (Exception ex)
                 {
@@ -148,8 +163,11 @@ namespace RO_TOKEN
 
 
 
-        public void ExportToken(String vmdkPath, String DestinationFile)
+        public async Task ExportToken(String vmdkPath, String DestinationFile)
         {
+
+
+
             String tokenFile = @"\\data\com.xd.ro.roapk\shared_prefs\XDUserToken.xml";
             VolumeManager volMgr = new VolumeManager();
             using VirtualDisk disk = VirtualDisk.OpenDisk(vmdkPath, FileAccess.Read);
@@ -168,23 +186,22 @@ namespace RO_TOKEN
                 int totalRead = 0;
                 while (totalRead < file.Length)
                 {
-                    totalRead += bootStream.Read(file, totalRead, file.Length - totalRead);
-
+                    totalRead +=await bootStream.ReadAsync(file, totalRead, file.Length - totalRead); 
                 }
                 using FileStream fileStream = File.Create(DestinationFile, (int)bootStream.Length);
-                bootStream.CopyTo(fileStream);
-                fileStream.Write(file, 0, (int)bootStream.Length);
+                await bootStream.CopyToAsync(fileStream);
+                await fileStream.WriteAsync(file, 0, (int)bootStream.Length);
 
             }
             else
             {
-                Console.WriteLine("\r\n [!] File {0} can not be found", tokenFile);
+                txt_log.AppendText($"\r\n [!] File {vmdkPath} can not be found");
             }
 
         }
 
 
-        public List<String> ExecCmd(String processPath, String arguments)
+        public async Task<List<String>> ExecCmd(String processPath, String arguments)
         {
             var psi = new ProcessStartInfo();
             psi.FileName = processPath;
@@ -196,29 +213,40 @@ namespace RO_TOKEN
             psi.RedirectStandardError = true;   //重定向标准错误输出
             using var process = Process.Start(psi);
 #pragma warning disable CS8602 // 解引用可能出现空引用。
-            process.WaitForExit();
+            await process.WaitForExitAsync();
 #pragma warning restore CS8602 // 解引用可能出现空引用。
             using StreamReader reader = process.StandardOutput;
-            string data = reader.ReadToEnd();
+            string data = await reader.ReadToEndAsync();
             using StreamReader errorReader = process.StandardError;
-            String error = errorReader.ReadToEnd();
-            String command = $"{LDCONSOLE} {psi.Arguments}";
+            String error = await errorReader.ReadToEndAsync();
+            String command = $"{psi.FileName} {psi.Arguments}";
             List<String> list = new List<string> { command, data, error };
             return list;
         }
 
-
-        public Dictionary<String, String[]> getRunning()
+        public async Task killAllAdbs()
         {
 
-            List<String> result = this.ExecCmd(LDCONSOLE, " list2"); 
+            List<String> result = await this.ExecCmd("taskkill", "/f /im  adb.exe");
+            txt_log.AppendText($" {result[0]}, {result[1]},{result[2]}");
+            await Task.Delay(2000);
+            result = await this.ExecCmd(ADBAPP, "devices");
+            txt_log.AppendText($" {result[0]}, {result[1]},{result[2]}");
+
+        }
+
+
+
+        public async Task<Dictionary<String, String[]>> getRunning()
+        {
+            Dictionary<String, String[]> devicesMap = new Dictionary<string, string[]>();
+            List<String> result = await this.ExecCmd(LDCONSOLE, " list2");
             if (!String.IsNullOrEmpty(result[2]))
             {
                 txt_export_path.AppendText($" {result[0]}, {result[1]},{result[2]}");
-                return null;
+                return devicesMap;
             }
-            List<String[]> list = result[1].Split("\r\n").Select(p => p.Split(",")).ToList();
-            Dictionary<String, String[]> devicesMap = new Dictionary<string, string[]>();
+            List<String[]> list = result[1].Split("\r\n").Select(p => p.Split(",")).ToList(); 
             foreach (var item in list)
             {
                 devicesMap.Add(item[0], item);
@@ -226,42 +254,47 @@ namespace RO_TOKEN
             return devicesMap;
         }
 
-        public String adbPushToken(String index, String tokenPath)
-        { 
-            var result=ExecCmd(LDCONSOLE, $" adb --index {index} --command \"push {tokenPath} /data/data/com.xd.ro.roapk/shared_prefs/XDUserToken.xml\""); 
+        public async Task<String> adbPushToken(String index, String tokenPath)
+        {
+            var result = await ExecCmd(LDCONSOLE, $" adb --index {index} --command \"push {tokenPath} /data/data/com.xd.ro.roapk/shared_prefs/XDUserToken.xml\"");
             String msg = $"{result[0]}--> {result[1]} {result[2]}";
             return msg;
         }
 
 
 
-        private void btn_import_token_Click(object sender, EventArgs e)
+        private async void btn_import_token_Click(object sender, EventArgs e)
         {
+
+
+ 
             bool init_ld = Init_Ldconsole();
+
             if (!init_ld)
             {
                 return;
             }
+
             HashSet<String> allVmdkFiles = getVmdkFiles();
-            List<String> allExist=new List<string>();
+            List<String> allExist = new List<string>();
             for (int i = cb_start.SelectedIndex; i <= cb_end.SelectedIndex; i++)
-            { 
+            {
                 String vmdk = Path.Join(txt_vms_folder.Text, "vms", "leidian" + i, "data.vmdk");
                 if (!allVmdkFiles.Contains(vmdk))
                 {
                     txt_log.AppendText("\r\n失败：" + vmdk + "模拟器不存在");
                     continue;
                 }
-                allExist.Add(i+"");
+                allExist.Add(i + "");
             }
-            if (allExist.Count==0)
+            if (allExist.Count == 0)
             {
-                MessageBox.Show("选择的范围内模拟器不存在！");
+                txt_log.AppendText("\r\n选择的范围内模拟器不存在！");
                 return;
             }
             while (true)
             {
-                var currentItems=  getRunning();
+                var currentItems = await getRunning();
                 int count = 0;
                 foreach (var item in allExist)
                 {
@@ -273,42 +306,51 @@ namespace RO_TOKEN
                     var currentMonitor = currentItems[item];
                     if (currentMonitor[6] == "-1")
                     {
-                        var execInfo=ExecCmd(LDCONSOLE, $" launch --index {currentMonitor[0]}");
+                        var execInfo = await ExecCmd(LDCONSOLE, $" launch --index {currentMonitor[0]}");
                         if (String.IsNullOrEmpty(execInfo[2]))
                         {
                             txt_log.AppendText($"启动{currentMonitor[0]}--{currentMonitor[1]}\r\n");
-                            Thread.Sleep(5000);
+                            await Task.Delay(5000);
                             continue;
                         }
-                        else { 
-                            MessageBox.Show($"启动{currentMonitor[0]}--{currentMonitor[1]}失败"); 
-                        } 
+                        else
+                        {
+                            txt_log.AppendText($"\r\n启动{currentMonitor[0]}--{currentMonitor[1]}失败");
+                        }
                     }
                     if (currentMonitor[4] != "-1")
                     {
                         count++;
                     }
                 }
-                if (count== allExist.Count)
+                if (count == allExist.Count)
                 {
                     break;
                 }
-                Thread.Sleep(5000);
+                await Task.Delay(5000);
             }
 
-      
+            await killAllAdbs();
+
+
+            for (int i = 30; i > 0; i--)
+            {
+                txt_log.AppendText($"\r\n 等待{i}秒");
+                await Task.Delay(1000);
+
+            }
 
             foreach (var i in allExist)
             {
-           
+
                 String exportFolder = Path.Join(txt_export_path.Text, "模拟器" + i);
                 String importFilename = Path.Join(exportFolder, "XDUserToken.xml");
                 if (!File.Exists(importFilename))
                 {
                     txt_log.AppendText("\r\n 失败：" + exportFolder + " 无效目录");
                     continue;
-                } 
-                String msg = adbPushToken(i, importFilename);
+                }
+                String msg = await adbPushToken(i, importFilename);
                 txt_log.AppendText("\r\n" + msg);
 
             }
